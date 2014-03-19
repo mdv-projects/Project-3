e4b63ef94d3e2e6862c576d130168d5928a31b19
Hector Baldaguez <Hbaldaguez@fullsail.edu>
2014-03-19 17:38:22 -0400

    Finished the array and added the array method.

--- BaldaguezRodriguez_Hector_Projec3.js
+++ BaldaguezRodriguez_Hector_Projec3.js
@@ -9,15 +9,16 @@ var myDecision = "yes"
 var huntBord = "Hunting Board";
 var userName;
 
-// The JSON data is in the json.js file
+// The JSON data is in the json.js file.
 
 
-// My Prompt and Proceduer
+// Title of the game.
 var say = function (message) {console.log(message);};
  
 alert("Welcome to the World of " + title)
 userName = prompt("Welcome Hunter.","What is your Hunter name?");
 
+// This will be the name of the users character.
 var userN = function (hunter) {
     
     say("Your Hunter Name is " + userName);
@@ -26,21 +27,61 @@ var userN = function (hunter) {
 userN(name)
 
 // Array function. Monsters to hunt. Mission Board.
+var monsters = function() {
+    
+var monstersNames = ["Gilbert the killer gunner", "The Scrapler"];
 
-var monstersNames = ["Gilbert the killer gunner", "The Scrapler, Shadrowing"];
-monstersNames[monstersNames.length] = "Gilbert the killer gunner"
+monstersNames[2] = "Nonsen the Blade";
 
+say("There are " + monstersNames.length + " missions on the " + huntBord + ".");
+
+say("The missions are:");
+     
+    for (var i = 0; i< monstersNames.length; i++)
+      
+     if (monstersNames[i] === "Gilbert the killer gunner") {
+        // Huntin types
+        say("The human to be hunted is " + monstersNames[i] + ".");
+    
+    } else {
+        
+        say("The monster to be hunted is " + monstersNames[i] + ".");
+    
+        var monsterList = monstersNames;     
+    };
+    
+    
+    return monsterList;
+};
+
+var mission = monsters();
+say("This are the monsters to hunt:");
+say(mission);
+
+// Type of waepon to choose. Array Method
+
+var weapons = ["Blade", "Bow", "Cannon"];
+
+say("The weapons you have available are:");
+say(weapons);
+
+var lastWeapon = weapons.pop();// Taking out the connon weapon that is not needed.
+
+say("We don't need the " + lastWeapon + ".")
+
+say("All we need for this mission is the " + weapons + ".")
 
 // Choosing mission from an array. In other words The mission board.
+// This will be the Boolean function to see if the battle is won or not.
 
-myDecision = confirm("Choose a Mission from the " + huntBord + ". Ok for Scrapler or Cancel for Gunner");
+myDecision = confirm("Choose a Mission from the " + huntBord + ". Ok for humans or Cancel for monsters");
 var decision = function (mission) {
     
     if (myDecision === true) {
         // What Mission to choose.
-        say("Let's go hunting.");
+        say("Let's take them down.");
     } else {
-        say("Let's take him down.");
+        say("Let's go hunting monsters.");
     };
     return;
 };
@@ -52,10 +93,10 @@ var attaking = function (hitPoints) {
     
     var attack = true;
     
-    while (hitPoints > -330) {
+    while (hitPoints > 0) {
         
         say("Slash him to death");
-        say("He's going down only " + hitPoints + " HP left");
+        say("He's going down. He has " + hitPoints + " HP to go.");
         attack = true
         
         attack = confirm("Time to hunt. OK to attack and Cancel to escape."); 
@@ -81,12 +122,12 @@ var exp = function (levelUp) {
     var levelInc = 150;
     var expPoints = 1200;
     while (levelInc <= expPoints){
-        console.log("Incrising the level of the weapon by " + levelInc + ". Level up to");
+        say("Incrising the level of the weapon by " + levelInc + ". Level up to");
         levelInc = levelInc + 150;
     
     };    
         for (var levelInc = 150, expPoints = 1200; levelInc <= expPoints; levelInc = levelInc + 150) {
-          console.log("You have added " + levelInc + " per level.");  
+          say("You have added " + levelInc + " per level.");  
         };
        return levelUp;
 };
